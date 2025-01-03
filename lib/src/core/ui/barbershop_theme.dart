8b6e79da5ba281912c9509adfce82c47fe6c02a4
João Felipe <joaofelipedev01@gmail.com>
2025-01-03 18:02:06 -0300

    Refactor BarbershopTheme and update dependencies in pubspec.lock

--- lib/src/core/ui/barbershop_theme.dart
+++ lib/src/core/ui/barbershop_theme.dart
@@ -3,31 +3,33 @@ import 'package:flutter/material.dart';
 
 sealed class BarbershopTheme {
   static const OutlineInputBorder _defaultInputBorder = OutlineInputBorder(
-      borderRadius: BorderRadius.all(
-        Radius.circular(8),
-      ),
-      borderSide: BorderSide(color: ColorsContants.grey));
+    borderRadius: BorderRadius.all(
+      Radius.circular(8),
+    ),
+    borderSide: BorderSide(color: ColorsContants.grey),
+  );
 
   static ThemeData themeData = ThemeData(
-      useMaterial3: true,
-      inputDecorationTheme: InputDecorationTheme(
-        filled: true,
-        fillColor: Colors.white,
-        labelStyle: const TextStyle(color: ColorsContants.grey),
-        border: _defaultInputBorder,
-        enabledBorder: _defaultInputBorder,
-        focusedBorder: _defaultInputBorder,
-        errorBorder: _defaultInputBorder.copyWith(
-            borderSide: const BorderSide(color: ColorsContants.red)),
-      ),
-      elevatedButtonTheme: ElevatedButtonThemeData(
-        style: ElevatedButton.styleFrom(
-          backgroundColor: ColorsContants.brow,
-          foregroundColor: Colors.white,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8),
-          ),
+    useMaterial3: true,
+    inputDecorationTheme: InputDecorationTheme(
+      filled: true,
+      fillColor: Colors.white,
+      labelStyle: const TextStyle(color: ColorsContants.grey),
+      border: _defaultInputBorder,
+      enabledBorder: _defaultInputBorder,
+      focusedBorder: _defaultInputBorder,
+      errorBorder: _defaultInputBorder.copyWith(
+          borderSide: const BorderSide(color: ColorsContants.red)),
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: ColorsContants.brow,
+        foregroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
         ),
       ),
-      fontFamily: FontConstants.fontFamily);
+    ),
+    fontFamily: FontConstants.fontFamily,
+  );
 }
